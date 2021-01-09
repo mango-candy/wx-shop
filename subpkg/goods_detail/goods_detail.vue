63fc7f9fd618d650da5cc340b9836896a5e6cc66
mango-candy <307688583@qq.com>
2021-01-09 22:48:54 +0800

    商品详情头部区域UI完成

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,10 +1,29 @@
 <template>
 	<view>
+    <!-- 轮播图区域 -->
 		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
 		  <swiper-item v-for="(item,i) in goods_info.pics" :key="i">
 		    <image :src="item.pics_big" @click="preview(i)"></image>
 		  </swiper-item>
       </swiper>
+      
+      <!-- 商品信息区域 -->
+      <view class="goods-info-box">
+        <!-- 商品价格 -->
+        <view class="price">￥{{goods_info.goods_price}}</view>
+        <!-- 信息主体区域 -->
+        <view class="goods-info-body">
+          <!-- 商品名称 -->
+          <view class="goods-name">{{goods_info.goods_name}}</view>
+          <!-- 收藏 -->
+          <view class="favi">
+            <uni-icons type="star" size="18" color="gray"></uni-icons>
+            <text>收藏</text>
+          </view>
+        </view>
+        <!-- 运费 -->
+        <view class="yf">快递：免运费</view>
+      </view>
 	</view>
 </template>
 
@@ -49,6 +68,45 @@ swiper {
   image {
     width: 100%;
     height: 100%;
+  }
+}
+// 商品信息区域的样式
+.goods-info-box {
+  padding: 10px;
+  padding-right: 0;
+
+  .price {
+    color: #c00000;
+    font-size: 18px;
+    margin: 10px 0;
+  }
+
+  .goods-info-body {
+    display: flex;
+    justify-content: space-between;
+
+    .goods-name {
+      font-size: 13px;
+      padding-right: 10px;
+    }
+    // 收藏区域
+    .favi {
+      width: 120px;
+      font-size: 12px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      border-left: 1px solid #efefef;
+      color: gray;
+    }
+  }
+
+  // 运费
+  .yf {
+    margin: 10px 0;
+    font-size: 12px;
+    color: gray;
   }
 }
 </style>
