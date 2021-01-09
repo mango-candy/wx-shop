33841a948e02b9781e6ca8df9d9c68df9aaa2cba
mango-candy <307688583@qq.com>
2021-01-09 21:32:40 +0800

    轮播图数据获取完成

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		测试SSH
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+		  <swiper-item v-for="(item,i) in goods_info.pics" :key="i">
+		    <image :src="item.pics_big"></image>
+		  </swiper-item>
+      </swiper>
 	</view>
 </template>
 
@@ -8,12 +12,34 @@
 	export default {
 		data() {
 			return {
-				
+        // 商品详情对象
+				goods_info:{}
 			};
-		}
+		},
+    onLoad(options){
+      // 获取跳转页面传递过来的商品id
+      const goods_id= options.goods_id
+      // 将跳转页面传递过来的商品id发送给服务器
+      this.getGoodsDetail(goods_id)
+    },
+    methods:{
+      async getGoodsDetail(goods_id){
+        const{data:res} = await uni.$http.get('/api/public/v1/goods/detail',{goods_id})
+        if(res.meta.status!==200) return uni.$showMsg()
+        this.goods_info=res.message
+       }   
+    },
+
 	}
 </script>
 
 <style lang="scss">
+swiper {
+  height: 750rpx;
 
+  image {
+    width: 100%;
+    height: 100%;
+  }
+}
 </style>
